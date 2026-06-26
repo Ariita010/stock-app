8bfce56655ee6ac1ef0afc2ac27314e33d2c30a1
Ariita010 <nick.arita@strathmore.edu>
2026-06-26 13:44:08 +0300

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,1 +1,27 @@
-// Write your Slider component here
+// Write your Slider component hereimport React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Slider = () => {
+    const [data, setData] = useState(null);
+    const apiKey = process.env.REACT_APP_API_KEY;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const options = {
+                    method: 'GET',
+                    url: 'https://yfapi.net/v6/finance/quote/marketSummary',
+                    headers: {
+                        'x-api-key': apiKey
+                    }
+                };
+
+                const response = await axios.request(options);
+                setData(response.data.marketSummaryResponse.result);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, [apiKey]);
